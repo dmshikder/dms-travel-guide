cd22f51aa8563410c877e60c6184236c4bfbfcb3
dmshikder <dmshikder@gmail.com>
2022-06-12 12:45:41 +0600

    not found page designed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from "./Pages/About/About";
 import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 import Services from "./Pages/Services/Services";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/services" element={<Services/>}> </Route>
         <Route path="/blogs" element={<Blogs/>}> </Route>
         <Route path="/about" element={<About/>}> </Route>
+        <Route path="*" element={<NotFound/>}> </Route>
         
       </Routes>
       <Footer></Footer>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+    return (
+        <div>
+            
+            <img className='img  w-100 h-full' src="https://www.gpkumar.com/wp-content/uploads/2020/05/HTML-404-Page.gif" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
